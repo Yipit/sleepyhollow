8cba33b3f61b709b1c322141090c04a80d65f4a2
Lincoln de Sousa <lincoln@comum.org>
2012-11-30 21:59:14 +0000

    Adding some doc to the bootstrap script and removing the libraries after running the tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+#
+# Handy script that cleans up the project folder, re-initializes the
+# build system, compiles everything and runs the tests for us.
+
 export DYLD_LIBRARY_PATH=`pwd`/tests
 
+# Cleanup
 > .gitignore
 git clean -df
+
+# Compilation
 ./autogen.sh
 make
+
+# Making it easier to run the tests
 cp lib/.libs/*.{dylib,so} tests
 cp python/.libs/*.{dylib,so} tests
 
+# Running the tests
 nosetests --verbosity=2 -s tests/test*.py
+
+# Cleaning up
+rm tests/*.{dylib,so}
