bcead41ad858c5493f61f0d8785a619df5b9a0c9
Lincoln de Sousa <lincoln@comum.org>
2012-12-03 01:26:06 +0000

    Using xvfb when the DISPLAY variable is not present
    The QWebPage QT class contains elements tied to the QtGui library,
    like font rendering stuff that needs to have an X server running, so
    this change runs the tests uxing the xvfb program to get a valid
    DISPLAY var.
    
    This is a temporary workaround for the issue #1

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,11 @@ cp lib/.libs/*.dylib tests
 cp python/.libs/*.so tests
 
 # Running the tests
-nosetests --verbosity=2 -s tests/test*.py
+if [ "x$DISPLAY" == "x" ]; then
+  xvfb-run --server-args="-screen 0 1024x768x24" nosetests --verbosity=2 -s tests
+else
+  nosetests --verbosity=2 -s tests
+fi
 
 # Cleaning up
 rm tests/*.{dylib,so}
