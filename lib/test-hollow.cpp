2f0aebb9a72d84c906a8550acdabd4a462b567e9
Lincoln de Sousa <lincoln@comum.org>
2012-12-03 18:32:29 +0000

    Improving the test-hollow program to make it easier to try the library while debugging

--- lib/test-hollow.cpp
+++ lib/test-hollow.cpp
@@ -9,7 +9,31 @@ main (int argc, char **argv)
     return 1;
   }
 
+  std::cout << "Creating the hollow instance" << std::endl;
   Hollow hollow;
-  std::cout << hollow.getUrlContent(argv[1]) << std::endl;
+
+  std::cout << "Making the request" << std::endl;
+  Response *resp = hollow.request("get", argv[1]);
+
+  if (resp == NULL) {
+    std::cout << "Not good bro, "
+              << "we got an error while trying to request the url "
+              << argv[1]
+              << "."
+              << std::endl;
+
+    // Giving a better sense of what actually happen
+    Error *error = Error::last();
+    if (error != NULL) {
+      std::cout << "Here's the error: "
+                << error->what()
+                << std::endl;
+      return 1;
+    }
+  }
+
+  std::cout << "Reading the response:" << std::endl
+            << "status: " << resp->getStatusCode()
+            << std::endl;
   return 0;
 }
