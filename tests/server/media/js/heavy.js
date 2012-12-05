0aab4fd3e550f94dd0fe40e5f289e5235dc9bd59
Gabriel Falcao <gabriel@nacaolivre.org>
2012-12-05 18:02:14 -0500

    improving load test

--- tests/server/media/js/heavy.js
+++ tests/server/media/js/heavy.js
@@ -1,13 +1,9 @@
 (function($){$(function(){
     var i = 0;
 
-    var timer = setInterval(function(){
-        $("body").prepend($('<div class="square" id="sq'+i+'"></div>'));
-        $('#sq'+i).animate({ 'margin': '10px', 'border-width': '40px', 'width': '100px', 'height': '100px'}, 500);
+    while (i <= 1000) {
+        $("body").append($('<div class="square"></div>'));
         i++;
-        if (i > 20) {
-            clearInterval(timer);
-        }
-    }, 500);
+    }
 
 })})(jQuery);
