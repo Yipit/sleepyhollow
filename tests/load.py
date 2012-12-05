202a899d7c551c962f1afdfbbddc9167eaea283f
Gabriel Falcao <gabriel@nacaolivre.org>
2012-12-05 14:55:18 -0500

    improving load test

--- tests/load.py
+++ tests/load.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from sure import expect
 from sleepyhollow import SleepyHollow
 
 http = SleepyHollow()
 
-list_of_things = []
 
 for x in range(101):
     r = http.get('http://localhost:5000/heavy', {'index': x})
-    list_of_things.append(id(r))
-    print r, r.url
-
-print list(set(list_of_things))
+    expect(r.url).to.equal('http://localhost:5000/heavy?index=%d' % x)
+    print r, r.url, id(r)
