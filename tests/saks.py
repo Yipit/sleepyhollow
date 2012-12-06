ae80247f7e0a67f28c5b8822f857253d71724ec5
Gabriel Falcao <gabriel@nacaolivre.org>
2012-12-06 11:29:01 -0500

    writting saks scrapper

--- tests/saks.py
+++ tests/saks.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from sure import expect
 from lxml import html as lhtml
 from sleepyhollow import SleepyHollow
 
 
 class Scraper(object):
-    base_url = 'http://www.saksfifthavenue.com'
+    base_url = 'http://m.saks.com'
 
     def __init__(self):
         self.http = SleepyHollow()
@@ -15,34 +14,50 @@ class Scraper(object):
         if to.startswith('http'):
             return to
 
-        return '/'.join(self.base_url, to.lstrip('/'))
+        return '/'.join([self.base_url, to.lstrip('/')])
 
     def get(self, path):
         return self.http.get(self.path(path))
 
     def get_root_links(self):
-        response = self.get('/search/SearchSale.jsp')
+        print "Getting root links..."
+        response = self.get('/eSearch.jsp?sid=127F38CAD8BC&N_Dim=0&bmSingle=N_Dim&N=1553&Ns=P_0_sort')
         dom = lhtml.fromstring(response.html)
-        return [l.attrib['href'] for l in dom.cssselect('a.leftNavText')]
+        return [l.attrib['href'] for l in dom.cssselect('#left-nav-content > div > a')]
+
+    def get_subcategory_links(self, link):
+        print "Getting category links on %r..." % link
+        response = self.get(link)
+        dom = lhtml.fromstring(response.html)
+        return [l.attrib['href'] for l in dom.cssselect('#left-nav-content > div > a')]
 
     def get_product_links(self, parent_url):
+        print "Getting product links on %r..." % parent_url
         response = self.get(parent_url)
         dom = lhtml.fromstring(response.html)
-        return [l.attrib['href'] for l in dom.cssselect('a.mainBlackText')]
+        return [l.attrib['href'] for l in dom.cssselect('.productRow > a')]
 
     def get_product_details(self, product_url):
-        name = dom.cssselect('h1.boldBlackText12')
+        print "Getting product details on %r..." % product_url
+
+        response = self.get(product_url)
+        dom = lhtml.fromstring(response.html)
+        img = dom.cssselect("#productMainImg")[0]
+        name = dom.cssselect('form h1')[0]
+        return dict(
+            name=name.text.strip(),
+            img=img.attrib['src'],
+        )
 
     def scrape(self):
         root_links = self.get_root_links()
+        for root in root_links:
+            subcategories = self.get_subcategory_links(root)
+            for subcat in subcategories:
+                products = self.get_product_links(subcat)
+                for prod in products:
+                    print self.get_product_details(prod)
 
 
-sl = SleepyHollow()
-r = sl.get('http://bananarepublic.gap.com/browse/product.do?pid=331815&locale=en_US&kwid=1&sem=false&sdReferer=http%3A%2F%2Fwww.bananarepublic.com%2Fproducts%2Fpetite-coats-sale.jsp')
-dom = lhtml.fromstring(r.html)
-
-imgs = dom.cssselect('#product_image')
-if len(imgs) != 1:
-    raise AssertionError('damn!')
-
-expect(imgs[0].attrib).to.have.key('src').being.equal('http://www3.assets-gap.com/webcontent/0005/198/392/cn5198392.jpg')
+s = Scraper()
+s.scrape()
